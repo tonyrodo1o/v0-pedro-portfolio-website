a34d06760fae631ea2706a197a012236d5c787bc
tonyrodo1o <pedrorodact01@gmail.com>
2026-05-21 12:11:04 +0100

    Update route.ts

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -1,5 +1,5 @@
 import { Resend } from 'resend';
-import { EmailTemplate } from '../../../components/email-template';
+//import { EmailTemplate } from '../../../components/email-template';
 import { NextResponse } from 'next/server';
 
 const resend = new Resend(process.env.RESEND_API_KEY);
