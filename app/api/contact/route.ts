3b8b94434d20aa32c71134ce6fb167c33e1f4ee9
tonyrodo1o <pedrorodact01@gmail.com>
2026-06-02 12:35:54 +0100

    Update route.ts

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -1,6 +1,7 @@
 /*import { Resend } from 'resend';
 //import { EmailTemplate } from '../../../components/email-template';
 import { NextResponse } from 'next/server';
+export const runtime = 'edge';
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
